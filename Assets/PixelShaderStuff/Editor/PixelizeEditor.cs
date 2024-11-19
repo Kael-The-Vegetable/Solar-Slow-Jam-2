415ad4b4c7c11671ba8d23e7bfd2841d8ad0b614
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-11-19 15:19:14 -0700

    Almost there with the shader

--- Assets/PixelShaderStuff/Editor/PixelizeEditor.cs
+++ Assets/PixelShaderStuff/Editor/PixelizeEditor.cs
@@ -7,8 +7,8 @@ public class PixelizeEditor : MonoBehaviour
 {
     private static readonly Camera cam = Camera.main;
 
-    private static readonly string renderTexturePath = "CameraInfo";
-    private static readonly RenderTexture texture = Resources.Load(renderTexturePath) as RenderTexture;
+    private static readonly string dataCamPath = "AbsorbingData";
+    private static readonly GameObject dataCam = Resources.Load(dataCamPath) as GameObject;
 
     private static readonly string canvasRawImage = "CameraCanvas";
     private static readonly GameObject canvas = Resources.Load(canvasRawImage) as GameObject;
@@ -19,26 +19,15 @@ public class PixelizeEditor : MonoBehaviour
         for (int i = 0; i < cam.transform.childCount; i++)
         {
             Transform child = cam.transform.GetChild(i);
-            if (child.name == "Absorbing Data" || child.name == "CameraCanvas(Clone)")
+            if (child.name == "AbsorbingData(Clone)" || child.name == "CameraCanvas(Clone)")
             { // delete child
                 DestroyImmediate(child.gameObject);
                 i--;
             }
         }
 
-        GameObject dataObj = new GameObject("Absorbing Data", typeof(Camera));
-        dataObj.transform.parent = cam.transform;
-
-        dataObj.transform.localPosition = Vector3.zero;
-        dataObj.transform.localEulerAngles = Vector3.zero;
-        dataObj.transform.localScale = Vector3.one;
-
-        Camera dataCam = dataObj.GetComponent<Camera>();
-
-        dataCam.fieldOfView = Camera.main.fieldOfView;
-        dataCam.targetTexture = texture;
-        
-        GameObject canvasObj = Instantiate(canvas, cam.transform);
+        Instantiate(dataCam, cam.transform);
+        Instantiate(canvas, cam.transform);
 
     }
 }
--- Assets/PixelShaderStuff/NewUnlitShader.shader
+++ Assets/PixelShaderStuff/NewUnlitShader.shader
@@ -1,19 +1,21 @@
-Shader "Hidden/PixelizationEffect"
+Shader "UI/BasicTexture"
 {
     Properties
     {
         _MainTex ("Texture", 2D) = "white" {}
-        _PixelSize ("Pixel Size", Float) = 10
     }
     SubShader
     {
-        Tags { "RenderType"="Opaque" }
+        Tags { "Queue"="Overlay" "RenderType"="Transparent" "IgnoreProjector"="True" "Canvas"="UI" }
         Pass
         {
             CGPROGRAM
             #pragma vertex vert
             #pragma fragment frag
             #include "UnityCG.cginc"
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
 
             struct appdata_t
             {
@@ -27,22 +29,17 @@ Shader "Hidden/PixelizationEffect"
                 float4 vertex : SV_POSITION;
             };
 
-            sampler2D _MainTex;
-            float _PixelSize;
-
             v2f vert (appdata_t v)
             {
                 v2f o;
                 o.vertex = UnityObjectToClipPos(v.vertex);
-                o.uv = v.uv;
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
                 return o;
             }
 
             float4 frag (v2f i) : SV_Target
             {
-                float2 screenUV = i.uv;
-                float2 pixelUV = floor(screenUV * _PixelSize) / _PixelSize;
-                return tex2D(_MainTex, pixelUV);
+                return tex2D(_MainTex, i.uv);
             }
             ENDCG
         }
