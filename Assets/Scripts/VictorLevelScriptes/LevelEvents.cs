fb082f3c1764c1fbbd0db7331db653aa0ad6b9ce
Supergoodra64 <144252131+Supergoodra64@users.noreply.github.com>
2024-11-19 17:08:03 -0700

    Door Lights set up

--- Assets/Scripts/VictorLevelScriptes/LevelEvents.cs
+++ Assets/Scripts/VictorLevelScriptes/LevelEvents.cs
@@ -4,7 +4,7 @@ public class LevelEvents : MonoBehaviour
 {
     int numberOfCrystals = 3;
 
-    [SerializeField] GameObject LightN01, LightN02, LightN03;
+    [SerializeField] GameObject lightN01, lightN02, lightN03;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,17 +22,17 @@ public class LevelEvents : MonoBehaviour
 
     void ActivateLightN01()
     {
-
+        lightN01.GetComponent<Renderer>().material.color = Color.green;
     }
 
 	void ActivateLightN02()
 	{
-
+        lightN02.GetComponent<Renderer>().material.color = Color.green;
 	}
 
 	void ActivateLightN03()
 	{
-
+        lightN03.GetComponent<Renderer>().material.color = Color.green;
 	}
 
     void OpenLevelExit()
